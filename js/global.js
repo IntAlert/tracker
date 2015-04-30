2a8e8d067e4844964133ba1320661f3f60ba757e
dlucas94 <Dan@L0-ICT-DEVs-MacBook-Air.local>
2015-04-30 15:38:16 +0100

    no signal stuff

---
+++ js/global.js
@@ -0,0 +1,9 @@
+var isConnected = false;
+var firebaseRef = new Firebase('https://crackling-fire-1447.firebaseio.com/');
+firebaseRef.child('.info/connected').on('value', function(connectedSnap) {
+  if (connectedSnap.val() === true) {
+    isConnected = true;
+  } else {
+    isConnected = false;
+  }
+});
