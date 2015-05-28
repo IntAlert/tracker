4d9ada708149523b0f4272811db69012e85018bd
Dan Lucas <darkogx@gmail.com>
2015-05-28 16:05:17 +0100

    js in separate files

---
+++ js/addtrip.js
@@ -0,0 +1,202 @@
+////////// DIALOG \\\\\\\\\\
+$(function() {
+    $( "#dialogTripAdded" ).dialog({
+        autoOpen: false,
+        modal: true,
+        dialogClass: "dlg-no-close",
+        buttons: {
+            "OK": function() {
+                $( this ).dialog( "close" );
+                window.location = "main.html";
+            }
+        }
+    });
+});
+
+$(function() {
+    $( "#dialogValLeave" ).dialog({
+        autoOpen: false,
+        modal: true,
+        dialogClass: "dlg-no-close",
+        buttons: {
+            "OK": function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+});
+
+$(function() {
+    $( "#dialogValReturn" ).dialog({
+        autoOpen: false,
+        modal: true,
+        dialogClass: "dlg-no-close",
+        buttons: {
+            "OK": function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+});
+
+$(function() {
+    $( "#dialogValDest" ).dialog({
+        autoOpen: false,
+        modal: true,
+        dialogClass: "dlg-no-close",
+        buttons: {
+            "OK": function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+});
+
+$(function() {
+    $( "#dialogValContact" ).dialog({
+        autoOpen: false,
+        modal: true,
+        dialogClass: "dlg-no-close",
+        buttons: {
+            "OK": function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+});
+
+$(function() {
+    $( "#dialogNoSignal" ).dialog({
+        autoOpen: false,
+        modal: true,
+        dialogClass: "dlg-no-close",
+        buttons: {
+            "OK": function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+});
+
+////////// MAIN SCRIPTS \\\\\\\\\\
+var myID = ""; // THIS IS GLOBAL!!!!!!!!!
+var uName = "";
+function authDataCallback(authData) {
+    if (authData) {
+    myID = authData.uid.substring(12,16);
+    //myID = sessionStorage.setItem("userid");
+    } 
+}             
+// Register the callback to be fired every time auth state changes
+var ref = new Firebase("https://crackling-fire-1447.firebaseio.com/");
+ref.onAuth(authDataCallback);
+
+var uemail = sessionStorage.getItem("email");
+
+function addTrip() {            
+    if(isConnected == false) {
+        $( '#dialogNoSignal' ).dialog('open');
+        return;
+    }
+
+    var leave = document.getElementById("leave").value;
+    var startDate = ($( "#leave" ).datepicker( "getDate" )); //get date for event
+    var back = document.getElementById("back").value;
+    var endDate = ($( "#back" ).datepicker( "getDate" )); //get date for event
+    var destination = document.getElementById("destination").value;
+    var mycontact = document.getElementById("contactdd").value;
+    var name = sessionStorage.getItem("name");
+    var lastname = sessionStorage.getItem("lastname");
+    var ref = new Firebase("https://crackling-fire-1447.firebaseio.com/");
+    var tripsRef = ref.child("trips");
+    var newTripsRef = tripsRef.push({
+        leave: leave,
+        destination: destination,
+        back: back,
+        uid: myID,
+        email: uemail,
+        contact: mycontact,
+        contactname: contact[mycontact][1],
+        contactlastname: contact[mycontact][2],
+        contactemail: contact[mycontact][3],
+        contactphone: contact[mycontact][4],
+        name: name,
+        lastname: lastname
+    }, function(){
+            createEvent(startDate, endDate, destination, contact[mycontact][2]);
+            $( '#dialogTripAdded' ).dialog('open');
+        });
+    var tripID = newTripsRef.key();
+
+}
+
+    var count = 1;
+    var contact = [];
+    var myText = "";
+    var ref = new Firebase("https://crackling-fire-1447.firebaseio.com/contacts");
+    ref.on("child_added", function(snapshot) {
+    newContact = snapshot.val();
+    contact[count] = new Array(4);
+    contact[count][1] = newContact.name;
+    contact[count][2] = newContact.fullname;
+    contact[count][3] = newContact.email;
+    contact[count][4] = newContact.phone;
+    document.getElementById("contactdd");
+    var optn = document.createElement("OPTION");
+        optn.text = contact[count][2] + " | " + contact[count][3];
+        optn.value = snapshot.key();
+        contactdd.options.add(optn);
+    count = count + 1; 
+    });
+
+function validateForm() {
+    if(document.tripdetails.leave.value == "") {
+        $( '#dialogValLeave' ).dialog('open');
+        document.tripdetails.leave.focus();
+        return false;
+    }
+    if(document.tripdetails.back.value == "") {
+        $( '#dialogValReturn' ).dialog('open');
+        document.tripdetails.back.focus();
+        return false;
+    }
+    if (document.tripdetails.destination.value == "Select a country") {
+        $( '#dialogValDest' ).dialog('open');
+        document.tripdetails.destination.focus();
+        return false;
+    }
+    if (document.tripdetails.contactdd.value == "Select a contact") {
+        $( '#dialogValContact' ).dialog('open');
+        document.tripdetails.contactdd.focus();
+        return false;
+    }
+    addTrip();
+}
+
+//Init datepickers
+$( "#back" ).datepicker({
+    dateFormat: "dd/mm/yy",
+    minDate: 0,
+    showOn: "button",
+    buttonImage: "icons2/datepicker.png",
+    buttonImageOnly: true,
+    onSelect: function(dateText){
+        $( "#back" ).text(dateText);
+    }
+});
+$( "#leave" ).datepicker({
+    dateFormat: "dd/mm/yy",
+    minDate: 0,
+    showOn: "button",
+    buttonImage: "icons2/datepicker.png",
+    buttonImageOnly: true,
+    onSelect: function(dateText){
+        $( "#leave" ).text(dateText);
+        var date1 = $("#leave").datepicker("getDate");          
+        var date = new Date( Date.parse( date1 ) );
+        date.setDate( date.getDate());       
+        var newDate = date.toDateString();
+        newDate = new Date( Date.parse( newDate ) );                     
+        $("#back").datepicker("option","minDate",newDate);
+    }
+});
---
+++ js/index.js
@@ -0,0 +1,71 @@
+////////// DIALOG \\\\\\\\\\
+$(function() {
+    $( "#dialogEmail" ).dialog({
+        autoOpen: false,
+        modal: true,
+        dialogClass: "dlg-no-close",
+        buttons: {
+            "OK": function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+});
+
+$(function() {
+    $( "#dialogPass" ).dialog({
+        autoOpen: false,
+        modal: true,
+        dialogClass: "dlg-no-close",
+        buttons: {
+            "OK": function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+});
+
+
+////////// MAIN SCRIPTS \\\\\\\\\\          
+function firstRun() {
+    var status = localStorage.getItem("status")
+    if (status === null || status.length === 0){
+        window.location="create.html";
+    }
+}
+
+function authMe() {
+username = document.getElementById("name").value;
+if(typeof(Storage) !== "undefined") {
+            sessionStorage.setItem("email", username);
+}
+password = document.getElementById("password").value;
+if (username === "") {
+    $( "#dialogEmail" ).dialog( "open" );
+   return;
+} else {
+if (password === "") {
+    $( "#dialogPass" ).dialog( "open" );
+   return;
+} else {
+    var ref = new Firebase("https://crackling-fire-1447.firebaseio.com/contacts");
+        ref.authWithPassword({
+            "email": username,
+            "password": password
+            }, authHandler);
+        }
+    }
+}
+
+// Create a callback to handle the result of the authentication
+function authHandler(error, authData) {
+if (error) {
+    alert("Login Failed! " + error);
+} else {
+    window.location="main.html";
+}
+}
+
+function forgotPass() {
+    window.location="forgot.html";
+}
